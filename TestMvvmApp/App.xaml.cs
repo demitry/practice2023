de87b89442b8c3fd2a878f6a32d9a0eb650e43b7
demitry <dpoluektov@gmail.com>
2023-04-06 17:53:27 +0300

    Use Commands and Queries in Store

--- TestMvvmApp/App.xaml.cs
+++ TestMvvmApp/App.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System.Windows;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Windows;
 using TestMvvmApp.Stores;
 using TestMvvmApp.ViewModels;
+using Toys.Domain.Commands;
+using Toys.Domain.Queries;
+using Toys.EntityFramework;
+using Toys.EntityFramework.Commands;
+using Toys.EntityFramework.Queries;
 
 namespace TestMvvmApp;
 
@@ -14,9 +20,34 @@ public partial class App : Application
     private readonly SelectedToyStore _selectedToyStore;
     private readonly ModalNavigationStore _modalNavigationStore;
 
+    private readonly ToysDbContextFactory _toysDbContextFactory;
+
+    private readonly IGetAllToysQuery _getAllToysQuery;
+    private readonly ICreateToyCommand _createToyCommand;
+    private readonly IUpdateToyCommand _updateToyCommand;
+    private readonly IDeleteToyCommand _deleteToyCommand;
+
     public App()
     {
-        _toysStore = new ToysStore();
+        string connectionString = "Data Source=Toys.db";
+        
+        DbContextOptions options = new DbContextOptionsBuilder()
+                .UseSqlite(connectionString)
+                .Options;
+        
+        _toysDbContextFactory = new ToysDbContextFactory(options);
+
+        _getAllToysQuery = new GetAllToysQuery(_toysDbContextFactory);
+        _createToyCommand = new CreateToyCommand(_toysDbContextFactory);
+        _updateToyCommand = new UpdateToyCommand(_toysDbContextFactory);
+        _deleteToyCommand = new DeleteToyCommand(_toysDbContextFactory);
+        
+        _toysStore = new ToysStore(
+            _getAllToysQuery, 
+            _createToyCommand, 
+            _updateToyCommand, 
+            _deleteToyCommand);
+        
         _selectedToyStore = new SelectedToyStore(_toysStore);
         _modalNavigationStore = new ModalNavigationStore();
     }
--- TestMvvmApp/Stores/ToysStore.cs
+++ TestMvvmApp/Stores/ToysStore.cs
@@ -1,19 +1,42 @@
-﻿using Toys.Domain.Models;
+﻿using Toys.Domain.Commands;
+using Toys.Domain.Models;
+using Toys.Domain.Queries;
 
 namespace TestMvvmApp.Stores
 {
     public class ToysStore
     {
+        private readonly IGetAllToysQuery _getAllToysQuery;
+        private readonly ICreateToyCommand _createToyCommand;
+        private readonly IUpdateToyCommand _updateToyCommand;
+        private readonly IDeleteToyCommand _deleteToyCommand;
+
+        public ToysStore(
+            IGetAllToysQuery getAllToysQuery, 
+            ICreateToyCommand createToyCommand, 
+            IUpdateToyCommand updateToyCommand, 
+            IDeleteToyCommand deleteToyCommand)
+        {
+            _getAllToysQuery = getAllToysQuery;
+            _createToyCommand = createToyCommand;
+            _updateToyCommand = updateToyCommand;
+            _deleteToyCommand = deleteToyCommand;
+        }
+
         public event Action<Toy> ToyAdded;
         public event Action<Toy> ToyUpdated;
 
-        public async Task Add(Toy toy) //Create
+        public async Task Add(Toy toy)
         {
+            await _createToyCommand.Execute(toy);
+
             ToyAdded?.Invoke(toy);
         }
 
         public async Task Update(Toy toy)
         {
+            await _updateToyCommand.Execute(toy);
+
             ToyUpdated?.Invoke(toy);
         }
     }
