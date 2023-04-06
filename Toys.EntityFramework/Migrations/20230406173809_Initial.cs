7786cb19e963b4f2732dcee7b7badf9df9ac364f
demitry <dpoluektov@gmail.com>
2023-04-06 20:47:01 +0300

    EF Initial Migration

---
+++ Toys.EntityFramework/Migrations/20230406173809_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Toys.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Toys",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Size = table.Column<string>(type: "TEXT", nullable: false),
+                    IsValid = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Toys", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Toys");
+        }
+    }
+}
---
+++ Toys.EntityFramework/ToysDesignTimeDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Toys.EntityFramework
+{
+    public class ToysDesignTimeDbContextFactory : IDesignTimeDbContextFactory<ToysDbContext>
+    {
+        public ToysDbContext CreateDbContext(string[] args = null)
+        {
+            return new ToysDbContext(new DbContextOptionsBuilder().UseSqlite("Data Source=Toys.db").Options);
+        }
+    }
+}
